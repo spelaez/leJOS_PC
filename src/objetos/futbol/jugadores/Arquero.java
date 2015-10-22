9a0010dea7f41c235594a75222920bf136124251
detonadorcito3000 <bladeforce@hotmail.es>
2015-10-21 21:09:37 -0500

    se volvio a crear la clase Arquero

--- src/objetos/futbol/jugadores/Arquero.java
+++ src/objetos/futbol/jugadores/Arquero.java
@@ -1,5 +1,18 @@
 package objetos.futbol.jugadores;
 
-public class Arquero {
-
+public class Arquero extends Futbolista {
+	public int TiempoSinGoles;
+	public byte dorsal;
+	
+public Arquero (String nombre, String posicion, int TiempoSinGoles,byte dorsal){
+	super(nombre,posicion);
+	this.TiempoSinGoles = TiempoSinGoles;
+	this.dorsal = dorsal;
+}
+public String toString(){
+	return super.toString()+" con el dorsal "+ dorsal+ " Tiempo Sin Goles "+TiempoSinGoles;
+}
+public boolean jugarConLasManos(){
+	return true;
+}
 }
