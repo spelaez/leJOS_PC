2ad3c4b8ac34ff870c8cfb906b489da2aaeecb4f
Santiago <santi_rua@hotmail.com>
2015-10-21 20:57:23 -0500

    Clase futbolista y sus metodos

---
+++ src/objetos/futbol/jugadores/Futbolista.java
@@ -0,0 +1,25 @@
+package objetos.futbol.jugadores;
+
+import java.util.ArrayList;
+
+public abstract class Futbolista {
+	private String nombre;
+	private final String posicion;
+	private ArrayList listaJugadas;
+	
+	public Futbolista(String nombre, String posicion){
+		this.nombre = nombre;
+		this.posicion = posicion;
+	}
+	
+	public String toString(){
+		return "El futbolista " + nombre + " juega de " + posicion;
+	}
+	
+	public boolean equals(Futbolista f){
+		return this.equals(f);
+	}
+	
+	public abstract boolean jugarConLasManos();
+	
+}
