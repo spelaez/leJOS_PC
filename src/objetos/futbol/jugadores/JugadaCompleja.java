69de80a042617cb6fa4cae905bc07e4613c254aa
pablo238 <pablo-238@hotmail.com>
2015-10-21 22:53:16 -0500

    Cree clase Jugadas complejas y estableci los atributos :)

---
+++ src/objetos/futbol/jugadores/JugadaCompleja.java
@@ -0,0 +1,10 @@
+package objetos.futbol.jugadores;
+
+public abstract class JugadaCompleja {
+	private String nombreJugada;
+	private final String fechaCreacion;
+	// private final UsuarioAdministrador Autor; 
+	//private ArrayList<JugadaPrimitiva> listaJugadas;
+	private String Explicacion;
+
+}
