fba7547e0665f1b08e5fc474f2bc7849776cddbc
pablo238 <pablo-238@hotmail.com>
2015-10-21 23:00:08 -0500

    Cree el constructor de la clase JugadaCompleja

--- src/objetos/futbol/jugadores/JugadaCompleja.java
+++ src/objetos/futbol/jugadores/JugadaCompleja.java
@@ -4,7 +4,16 @@ public abstract class JugadaCompleja {
 	private String nombreJugada;
 	private final String fechaCreacion;
 	// private final UsuarioAdministrador Autor; 
-	//private ArrayList<JugadaPrimitiva> listaJugadas;
+	//private ArrayList<JugadaPrimitiva> Jugada;
 	private String Explicacion;
+	
+	public JugadaCompleja(String nombreJugada,String fechaCreacion,UsuarioAdministrador Autor,ArrayList<JugadaPrimitiva> Jugada,String Explicacion){
+		this.nombreJugada=nombreJugada;
+		this.fechaCreacion=fechaCreacion;
+		this.Autor=Autor;
+		this.Jugada=Jugada;
+		this.Explicacion=Explicacion;
+		
+	}
 
 }
