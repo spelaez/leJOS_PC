1dac6e22b6b0a15a4f5aab5797d87d11557e58a3
jhonemu <jhonmurillousuga33@gmail.com>
2015-10-25 13:22:31 -0500

    agregue el arraylist

--- src/objetos/futbol/jugadores/JugadaComplejaOfensiva.java
+++ src/objetos/futbol/jugadores/JugadaComplejaOfensiva.java
@@ -1,7 +1,6 @@
-
 package objetos.futbol.jugadores;
 import  objetos.futbol.robots.JugadaPrimitiva;
-
+import java.util.ArrayList;
 public class JugadaComplejaOfensiva extends JugadaCompleja {
 
 	public JugadaComplejaOfensiva(String nombreJugada,String fechaCreacion,UsuarioAdministrador Autor,ArrayList<JugadaPrimitiva> Jugada,String Explicacion){
