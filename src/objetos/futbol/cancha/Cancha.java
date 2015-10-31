7ae03f019e6b741c812897b1e95464438a13b0f5
Santiago <santi_rua@hotmail.com>
2015-10-30 23:11:39 -0500

    Cambie los valores de cm a dm

--- src/objetos/futbol/cancha/Cancha.java
+++ src/objetos/futbol/cancha/Cancha.java
@@ -1,13 +1,14 @@
 package objetos.futbol.cancha;
 
 public class Cancha {
-//Medidas en CM
-	static final int LARGO_CANCHA = 122;
-	static final int ANCHO_CANCHA = 183;
-	static final int ARCO_ANCHO = 8;
-	static final int ARCHO_LARGO = 45;
-	static final int ZONA_ARCO_ANCHO = 45;
-	static final int CAMPO = 93;
-	static final int BORDES_EXTERIORES = 300;
-	static final int ZONA_CENTRAL = 93;
+//Medidas en dm
+	static final float LARGO_CANCHA = 12.2F;
+	static final float ANCHO_CANCHA = 18.3F;
+	static final float ARCO_ANCHO = 0.8F;
+	static final float ARCO_LARGO = 4.5F;
+	static final float ZONA_ARCO_ANCHO = 4.5F;
+	static final float CAMPO = 9.3F;
+	static final float BORDES_EXTERIORES = 3F;
+	static final float ZONA_CENTRAL = 9.3F;
+	static final float ZONA_SUP_INF = 3.85F;
 }
