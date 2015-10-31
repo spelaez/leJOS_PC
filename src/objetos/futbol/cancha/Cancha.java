40d2af07fca3e70774f9b9eaf46bd679aec6e261
Santiago <santi_rua@hotmail.com>
2015-10-30 22:05:34 -0500

    Creada la clase cancha y añadidas algunas constantes

---
+++ src/objetos/futbol/cancha/Cancha.java
@@ -0,0 +1,13 @@
+package objetos.futbol.cancha;
+
+public class Cancha {
+//Medidas en CM
+	static final int LARGO_CANCHA = 122;
+	static final int ANCHO_CANCHA = 183;
+	static final int ARCO_ANCHO = 8;
+	static final int ARCHO_LARGO = 45;
+	static final int ZONA_ARCO_ANCHO = 45;
+	static final int CAMPO = 93;
+	static final int BORDES_EXTERIORES = 300;
+	static final int ZONA_CENTRAL = 93;
+}
