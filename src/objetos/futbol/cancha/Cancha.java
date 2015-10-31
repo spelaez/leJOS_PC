9df0f6e3c1841879d276e35ce49112fe903637f9
Santiago <santi_rua@hotmail.com>
2015-10-30 23:43:04 -0500

    Añadi metodos para inicializar la cancha y actualizar una posicion de la cancha

--- src/objetos/futbol/cancha/Cancha.java
+++ src/objetos/futbol/cancha/Cancha.java
@@ -11,4 +11,33 @@ public class Cancha {
 	static final float BORDES_EXTERIORES = 3F;
 	static final float ZONA_CENTRAL = 9.3F;
 	static final float ZONA_SUP_INF = 3.85F;
+	static String[][] rep;
+	
+	public void inicializarCancha(){
+		rep = new String[(int)LARGO_CANCHA][(int)ANCHO_CANCHA];
+		for(int i=0; i<(int)LARGO_CANCHA; i++){
+			for(int j=0; j<(int)ANCHO_CANCHA; j++){
+				rep[i][j]= " ";
+			}
+		}
+		for(int j=0; j<(int)ANCHO_CANCHA; j++){
+			rep[0][j] = "#";
+			rep[4][j] = "#";
+			rep[8][j] = "#";
+			rep[(int)(LARGO_CANCHA-1)][j] = "#";
+		}
+		for(int i=0; i<(int)LARGO_CANCHA; i++){
+			rep[i][0] = "#";
+			rep[i][(int)(ANCHO_CANCHA-1)] = "#";
+		}
+		for(int i=4; i<=8; i++){
+			rep[i][5] = "#";
+			rep[i][14] = "#";
+		}
+	}
+	
+	public void actualizarPosicion(int i, int j, int anti, int antj){
+		rep[i][j] = "X";
+		rep[anti][antj] = " ";
+	}
 }
