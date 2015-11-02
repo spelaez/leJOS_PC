480c518c18189140a9c9f684d2df95e215aafc8b
Santiago <santi_rua@hotmail.com>
2015-11-01 23:37:24 -0500

    Cambiado girar por girarDerecha y girarIzquierda

--- src/objetos/futbol/robots/Robot.java
+++ src/objetos/futbol/robots/Robot.java
@@ -9,16 +9,18 @@ public class Robot {
 	private MoverAtras retroceder;
 	private Chutar chutar;
 	private Chutar patear;
-	private Girar girar;
+	private Girar girarDerecha;
+	private Girar girarIzquierda;
 	
-	public Robot(Futbolista jugador, MoverAdelante trotar, MoverAdelante correr, MoverAtras retroceder, Chutar chutar, Chutar patear, Girar girar){
+	public Robot(Futbolista jugador, MoverAdelante trotar, MoverAdelante correr, MoverAtras retroceder, Chutar chutar, Chutar patear, Girar girarDerecha, Girar girarIzquierda){
 		this.jugador = jugador;
 		this.trotar = trotar;
 		this.correr = correr;
 		this.retroceder = retroceder;
 		this.chutar = chutar;
 		this.patear = patear;
-		this.girar = girar;
+		this.girarDerecha = girarDerecha;
+		this.girarIzquierda = girarIzquierda;
 	}
 	
 	public Robot(Futbolista jugador){
@@ -28,7 +30,8 @@ public class Robot {
 		this.retroceder = new MoverAtras(1);
 		this.chutar = new Chutar(1, 10);
 		this.patear = new Chutar(2,10);
-		this.girar = new Girar(1, 20);
+		this.girarDerecha = new Girar(1, 45);
+		this.girarIzquierda = new Girar(1, -45);
 	}
 	
 	public Futbolista getJugador(){
@@ -51,8 +54,12 @@ public class Robot {
 		return retroceder.getIdJugada();
 	}
 	
-	public int ejecutarGirar(){
-		return girar.getIdJugada();
+	public int ejecutarGirarDerecha(){
+		return girarDerecha.getIdJugada();
+	}
+	
+	public int ejecutarGirarIzquierda(){
+		return girarIzquierda.getIdJugada();
 	}
 	
 	public int ejecutarChutar(){
