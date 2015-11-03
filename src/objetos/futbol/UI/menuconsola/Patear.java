b531a9a91baf0272fbaa0b28c71acc35e2d6fce5
pablo238 <pablo-238@hotmail.com>
2015-11-03 17:08:02 -0500

    Cree clase Patear del menu de consola con 3 metodos, constructor,toString y ejecutar

---
+++ src/objetos/futbol/UI/menuconsola/Patear.java
@@ -0,0 +1,28 @@
+package objetos.futbol.UI.menuconsola;
+
+import java.io.IOException;
+
+import objetos.futbol.UI.Main;
+
+public class Patear extends OpcionDeMenu{
+	
+	public Patear(Categoria categoria){
+		super(categoria);
+	}
+	
+	@Override
+	public void ejecutar(){
+		System.out.print("---------------------------------------------------\n"+this);
+		try{
+		Main.dos.writeInt(Main.patear.getIdJugada());
+		}
+		catch(IOException e){
+			System.out.print("No se pudo ejecutar la jugada");
+		}
+	}
+	
+	@Override
+	public String toString(){
+		return "Patear";
+	}
+}
