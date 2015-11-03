911a63cbdd5aa9fba4195b5554bdfe62cc71a8fb
pablo238 <pablo-238@hotmail.com>
2015-11-03 18:15:23 -0500

    Cree clase ImprimirJugadores del menu consola con sus metodos tostring,constructor y ejecutar

---
+++ src/objetos/futbol/UI/menuconsola/ImprimirJugadores.java
@@ -0,0 +1,24 @@
+package objetos.futbol.UI.menuconsola;
+
+import objetos.futbol.UI.Main;
+
+public class ImprimirJugadores extends OpcionDeMenu {
+
+	public ImprimirJugadores(Categoria categoria){
+		super(categoria);
+	}
+	
+	@Override
+	public void ejecutar(){
+		System.out.print("---------------------------------------------------\n"+this);
+		
+		for(int i=0; i<Main.listaJugadores.size(); i++){
+			System.out.println("\t\t"+Main.listaJugadores.get(i));
+		}
+	}
+	
+	@Override
+	public String toString(){
+		return "Lista de jugadores";
+	}
+}
