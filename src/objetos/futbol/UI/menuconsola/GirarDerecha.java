2fbcd6402c0877f26db8679d4beb84a1b7f8814d
pablo238 <pablo-238@hotmail.com>
2015-11-03 17:21:48 -0500

    Cree la clase GirarDerecha de el menu consola con sus respectivos metodos constructor,toString y ejecutar

---
+++ src/objetos/futbol/UI/menuconsola/GirarDerecha.java
@@ -0,0 +1,27 @@
+package objetos.futbol.UI.menuconsola;
+
+import java.io.IOException;
+
+import objetos.futbol.UI.Main;
+public class GirarDerecha extends OpcionDeMenu{
+	
+	public GirarDerecha(Categoria categoria){
+		super(categoria);
+	}
+	
+	@Override
+	public void ejecutar(){
+		System.out.print("---------------------------------------------------\n"+this);
+		try{
+		Main.dos.writeInt(Main.girarDerecha.getIdJugada());
+		}
+		catch(IOException e){
+			System.out.print("No se pudo ejecutar la jugada");
+		}
+	}
+	
+	@Override
+	public String toString(){
+		return "Girar a la derecha";
+	}
+}
