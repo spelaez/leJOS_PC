7f38b5a1add746fbbde7bad57129e9974bbae93b
Santiago <santi_rua@hotmail.com>
2015-11-04 21:59:38 -0500

    Definidos metodos asignarMenu y agregarPErmisos

--- src/objetos/futbol/varios/UsuarioGeneral.java
+++ src/objetos/futbol/varios/UsuarioGeneral.java
@@ -1,15 +1,19 @@
 package objetos.futbol.varios;
 
 import objetos.futbol.UI.menuconsola.MenuDeConsola;
+import java.util.ArrayList;
 
 public class UsuarioGeneral implements Usuario
 {
 	private String nombre;
 	private String clave;
 	public MenuDeConsola menu = new MenuDeConsola();
+	public ArrayList<Integer> permisos = new ArrayList<>();
+	
 	public UsuarioGeneral(String nombre, String clave) {
 		this.nombre = nombre;
 		this.clave = clave;
+		agregarPermisos();
 	}
 	
 	public void setNombre(String nombre){
@@ -31,5 +35,16 @@ public class UsuarioGeneral implements Usuario
 	@Override
 	public void lanzarMenu() {
 		menu.lanzarMenu();
+	}
+	
+	public void agregarPermisos(){
+		//TODO: codigo para agregar los permisos cuando estén definidos
+	}
+	
+	public void asignarMenu(){
+		menu.limpiar();
+		for(int i=0; i<permisos.size(); i++){
+			menu.anadirOpcion(MenuDeConsola.opciones2.get(permisos.get(i)));
+		}
 	}
 }
