2e7fc944543b51474dbc6af90f54fe482ab15aca
Santiago <santi_rua@hotmail.com>
2015-11-12 20:13:09 -0500

    Arreglado registrarTiempoUltimoGol

--- src/objetos/futbol/UI/menuconsola/RegistrarTiempoUltimoGol.java
+++ src/objetos/futbol/UI/menuconsola/RegistrarTiempoUltimoGol.java
@@ -2,32 +2,32 @@ package objetos.futbol.UI.menuconsola;
 
 import objetos.futbol.UI.Main;
 /**
- * Clase que define la opción  que registra el tiempo del ultimo gol
+ * Clase que define la opciï¿½n  que registra el tiempo del ultimo gol
  * @author Jhon Eider Murrillo
  *
  */
 public class RegistrarTiempoUltimoGol extends OpcionDeMenu {
 	/**
-	 * Constructor que accesa al tipo de categoria que tiene la opción registrar tiempo del ultimo gol
+	 * Constructor que accesa al tipo de categoria que tiene la opciï¿½n registrar tiempo del ultimo gol
 	 * @param categoria
 	 */
 	public RegistrarTiempoUltimoGol(Categoria categoria){
 		super(categoria);
 	}//Cierre del constructor
 	/**
-	 * Método que se ejecuta para mostrar el tiempo del ultimo gol
+	 * Mï¿½todo que se ejecuta para mostrar el tiempo del ultimo gol
 	 */
 	@Override
 	public void ejecutar(){
 		System.out.print("---------------------------------------------------\n"+this+"\n");
-		Main.tUltimoGol = System.currentTimeMillis();
-	}//Cierre del método
+		Main.tUltimoGol = System.currentTimeMillis()-Main.tInicio;
+	}//Cierre del mï¿½todo
 	/**
-	 * Método sobreescrito de object que esta asignado por defecto, modificado para devolver el tipo de opción
-	 * @return Retorna el tipo de opción
+	 * Mï¿½todo sobreescrito de object que esta asignado por defecto, modificado para devolver el tipo de opciï¿½n
+	 * @return Retorna el tipo de opciï¿½n
 	 */
 	@Override
 	public String toString(){
 		return "Registrar el tiempo del ultimo gol";
-	}//Cierre del método
+	}//Cierre del mï¿½todo
 }//Cierre de la clase
