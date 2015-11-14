6fb66d9d928a5367d697741294b90e3681f41928
Santiago <santi_rua@hotmail.com>
2015-11-14 14:42:00 -0500

    Correciones en canchaposicion

--- src/objetos/futbol/cancha/Cancha.java
+++ src/objetos/futbol/cancha/Cancha.java
@@ -2,7 +2,6 @@ package objetos.futbol.cancha;
 
 import java.io.IOException;
 
-import objetos.futbol.UI.Main;
 import objetos.futbol.jugadores.Arquero;
 import objetos.futbol.robots.Robot;
 
@@ -53,18 +52,17 @@ public class Cancha {
 	}
 	
 	public void actualizarPosicion(int x, int y, Robot r) throws IOException{//Recibido movimiento del robot en milimetros
-		/*try{
+		try{
 			if(r.getJugador() instanceof Arquero){
 			rep[Aposx/61][Aposy/61] = repClone[Aposx/61][Aposy/61];
-			Aposx += x;
-			Aposy += y;
+			Aposx = x;
+			Aposy = y;
 			rep[Aposx/61][Aposy/61] = "A";
 			try{
 			comprobarLimite();
 			}
 			catch(FieldLimitExceededException e){
 				System.out.println(e.getMessage());
-				Main.dos.writeInt(0);
 				rep[Aposx/61][Aposy/61] = repClone[Aposx/61][Aposy/61];
 				Aposx = 0;
 				Aposy = LARGO_CANCHA-1;
@@ -73,19 +71,28 @@ public class Cancha {
 		}
 		else{
 			rep[Aposx/61][Aposy/61] = repClone[Aposx/61][Aposy/61];
-			Dposx += x;
-			Dposy += y;
+			Dposx = x;
+			Dposy = y;
 			rep[Aposx/61][Aposy/61] = "D";
 		}
 		}catch(IndexOutOfBoundsException e){
 			System.out.println("El Robot se ha salido de la cancha, porfavor reposicionarlo");
 		}
-		System.out.println("El robot ha cambiado a la posicion :"+ (Aposx/61) +" "+(Aposy/61));*/
+		System.out.println("El robot ha cambiado a la posicion :"+ (Aposx/61) +" "+(Aposy/61));
 	}
 	
 	public void comprobarLimite() throws FieldLimitExceededException{
 		if(Aposx > ANCHO_CANCHA/2*61){
 			throw new FieldLimitExceededException();
+		}
+	}
+
+	public void mostrarCancha(){
+		for(int i = 0; i < ANCHO_CANCHA; i++){
+			for(int j = 0; j < LARGO_CANCHA; j++){
+				System.out.print(rep[i][j]);
+			}
+			System.out.print("\n");
 		}
 	}
 }
--- src/objetos/futbol/cancha/FieldLimitExceededException.java
+++ src/objetos/futbol/cancha/FieldLimitExceededException.java
@@ -8,7 +8,7 @@ public class FieldLimitExceededException extends Exception{
 	private static final long serialVersionUID = -667246155539088747L;
 
 	public FieldLimitExceededException(){
-		super("Robot arquero ha sobrepasado la mitad de la cancha");
+		super("Robot arquero ha sobrepasado la mitad de la cancha, por favor muevalo de nuevo a su posicion inicial");
 	}
 	
 
