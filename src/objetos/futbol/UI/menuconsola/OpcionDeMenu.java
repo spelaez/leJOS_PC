40a8cd69a24f0990e83c0011bb4a17744d84e47c
Santiago <santi_rua@hotmail.com>
2015-11-30 23:32:23 -0500

    añadido al constructor de opcion de menu un AbstractAction

--- src/objetos/futbol/UI/menuconsola/OpcionDeMenu.java
+++ src/objetos/futbol/UI/menuconsola/OpcionDeMenu.java
@@ -1,6 +1,16 @@
 package objetos.futbol.UI.menuconsola;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.beans.PropertyChangeListener;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.JMenuItem;
+
+import objetos.futbol.UI.GUI.OyenteMenu;
 
 /**
  * Clase abastracta que lista las opciones de men� por categorias
@@ -19,6 +29,14 @@ public abstract class OpcionDeMenu extends JMenuItem{
 	public OpcionDeMenu(Categoria categoria){
 		super.setText(toString());
 		this.categoria = categoria;//Cierre del m�todo
+		this.addActionListener(new AbstractAction("Accion de item") {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				OpcionDeMenu o = (OpcionDeMenu) e.getSource();
+				o.ejecutar();
+			}
+		});
 	/**
 	 * Metodo abastracto que sera implementado en otras clases
 	 */
