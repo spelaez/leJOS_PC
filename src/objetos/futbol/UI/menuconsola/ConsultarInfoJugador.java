6fb47bdc95416a5ca44b735ac1778b96e70b690e
pablo238 <pablo-238@hotmail.com>
2015-12-03 01:51:27 -0500

    Lista ventana ConsultarinfoJugador

--- src/objetos/futbol/UI/menuconsola/ConsultarInfoJugador.java
+++ src/objetos/futbol/UI/menuconsola/ConsultarInfoJugador.java
@@ -2,14 +2,36 @@ package objetos.futbol.UI.menuconsola;
 import objetos.futbol.UI.Main;
 import objetos.futbol.UI.menuconsola.ImprimirJugadores;
 import objetos.futbol.jugadores.*;
+
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 /**
  * Clase que define la opción de consultar la información de un jugador
  * @author Santiago Pelaez
  *
  */
-public class ConsultarInfoJugador extends OpcionDeMenu{
+public class ConsultarInfoJugador extends OpcionDeMenu implements ActionListener{
+	JFrame jugadores= new JFrame("Seleccion jugador");
+	Container contenedor;
+	JPanel p1;
+	JButton aceptar;
+	ButtonGroup bg1;
+	String info;
 	/**
 	 * Constructor que accesa al tipo de categoria que tiene la opcion de consutar la informacion de un jugador
 	 * @param categoria
@@ -21,7 +43,24 @@ public class ConsultarInfoJugador extends OpcionDeMenu{
 	 * Metodo sobreescrito de opciones de menu que ejecuta el menu para consultar la informacion de un jugador
 	 */
 	@Override
-	public void ejecutar(){}
+	public void ejecutar(){
+		contenedor = jugadores.getContentPane();
+		p1 = new JPanel();
+		p1.setLayout(new GridLayout(12,1));
+		contenedor.add(p1);
+		aceptar = new JButton("Aceptar");
+		aceptar.addActionListener(this);
+		JLabel ind1 = new JLabel("Escoja un jugador");
+		p1.add(ind1);
+		bg1 = new ButtonGroup();
+		generarRadioButton(Main.listaJugadores);
+		p1.add(aceptar);
+		jugadores.setSize(500,500);
+		jugadores.setVisible(true);
+		
+		
+	}
+		
 		/*new ImprimirJugadores(Categoria.SISTEMA).ejecutar();
 		if(Main.listaJugadores.isEmpty()) return;
 		System.out.print("---------------------------------------------------\n"+this+"\n");
@@ -68,5 +107,24 @@ public class ConsultarInfoJugador extends OpcionDeMenu{
 	public String toString(){
 		return "Consultar informacion de un jugador";
 	}//Cierre del metodo
-}//Cierre de la clase
+	
+	public void actionPerformed(ActionEvent arg0) {
+		jugadores.dispose();
+		JOptionPane.showMessageDialog(this.jugadores, info);
+	}
+	public void generarRadioButton(ArrayList<Futbolista> j){
+		for(int i=0; i<j.size(); i++){
+			JRadioButton z = new JRadioButton(j.get(i).getNombre());
+			z.setName(i+"");
+			z.addItemListener(new ItemListener(){
+				public void itemStateChanged(ItemEvent e) {
+					if(z.isSelected()){
+						info = Main.listaJugadores.get(Integer.parseInt(z.getName())).toString();
+					}
+				}
+			});
+			bg1.add(z);
+			p1.add(z);
+		}
+}}//Cierre de la clase
 
